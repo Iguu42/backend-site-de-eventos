b52b7bdd7d2b7d75fc1f2110c0cdfbaafef2d000
Igor Félix <37526093+Iguu42@users.noreply.github.com>
2024-05-04 12:56:41 -0300

    update queues

--- src/lib/queue.lib.ts
+++ src/lib/queue.lib.ts
@@ -1,18 +1,43 @@
-import Bull from "bull";
+import Bull, { Job } from "bull";
 import 'dotenv/config';
 import { PurchaseOrderUseCase } from "../usecases/purchaseOrder.usecases";
 
-const queue = new Bull('purchaseOrder', {
-  redis: {
-    host: process.env.REDIS_HOST as string,
-    port: parseInt(process.env.REDIS_PORT as string)
+interface QueueConfig {
+  name: string;
+  processJob: (job: Job) => Promise<any>;
+};
+
+const queuesConfig: QueueConfig[] = [
+  {
+    name: 'purchaseOrder',
+    processJob: async (job) => {
+      const purchaseOrderUseCase = new PurchaseOrderUseCase();
+      console.log('Processing job:', job.data);
+      return await purchaseOrderUseCase.create(job.data);
+    }
   }
-});
+  // Adicione outras filas aqui
+];
 
-queue.process(async (job) => {
-    const purchaseOrderUseCase = new PurchaseOrderUseCase();
-    console.log('Processing job:', job.data);
-    return await purchaseOrderUseCase.create(job.data);
-});
+const queues: Record<string, Bull.Queue> = {};
 
-export default queue;
+export function createQueues(): void {
+  for (const queueConfig of queuesConfig) {
+    const { name, processJob } = queueConfig;
+    queues[name] = new Bull(name, {
+      redis: {
+        host: process.env.REDIS_HOST as string,
+        port: parseInt(process.env.REDIS_PORT as string)
+      }
+    });
+    queues[name].process(processJob);
+  };
+};
+
+export function getQueue(name: string): Bull.Queue {
+  if (!queues[name]) {
+    throw new Error(`Queue ${name} does not exist.`);
+  };
+
+  return queues[name];
+};
--- src/routes/purchaseOrder.routes.ts
+++ src/routes/purchaseOrder.routes.ts
@@ -1,13 +1,13 @@
 import { FastifyInstance } from "fastify";
 import { PurchaseOrderAndTicketsCreate } from "../interfaces/purchaseOrder.interface";
-import { PurchaseOrderUseCase } from "../usecases/purchaseOrder.usecases";
-import queue from "../lib/queue.lib";
-
-const purchaseOrderUseCase = new PurchaseOrderUseCase();
+import { createQueues, getQueue } from "../lib/queue.lib";
 
 export async function purchaseOrderRoutes(fastify: FastifyInstance) {
+  createQueues();
+
   fastify.post<{ Body: PurchaseOrderAndTicketsCreate }>('/', async (req, reply) => {
     try {
+      const queue = getQueue('purchaseOrder');
       const data = await queue.add({...req.body});
       const result = await data.finished();
       reply.code(201).send(result);
@@ -15,4 +15,4 @@ export async function purchaseOrderRoutes(fastify: FastifyInstance) {
       reply.code(400).send(error);
     }
   });
-}
+};
