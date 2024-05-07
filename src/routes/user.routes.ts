569e8a4c8ad994e279aed6bfdf9386955da1621e
Igor Félix <37526093+Iguu42@users.noreply.github.com>
2024-05-07 00:42:46 -0300

    created CRUD to Users

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from "fastify";
 import { UserUseCase } from "../usecases/user.usecase";
-import { UserCreate } from "../interfaces/user.interface";
+import { User,UserCreate, UserUpdate } from "../interfaces/user.interface";
 import { UserRepositoryPrisma } from "../repositories/user.repository";
 
 const userRepository = new UserRepositoryPrisma();
@@ -8,16 +8,43 @@ const userUseCase = new UserUseCase(userRepository);
 
 export async function userRoutes(fastify: FastifyInstance) {
     registerUserRoute(fastify);
+    updateUserRoute(fastify);
+    deleteUserRoute(fastify);
 }
 
 function registerUserRoute(fastify: FastifyInstance) {
     fastify.post<{ Body: UserCreate }>('/', async (req, reply) => {
-        const { externalId, name, email } = req.body;
+        const { externalId, firstName ,lastName, email } = req.body;
         try {
-            const data = await userUseCase.create({ externalId, name, email });
+            const data = await userUseCase.create({ externalId, firstName, lastName, email });
             reply.code(201).send(data);
         } catch (error) {
             reply.code(400).send(error);
+        }
+    });
+};
+
+function updateUserRoute(fastify: FastifyInstance) {
+    fastify.patch<{ Body: UserUpdate, Params: {id: string} }>('/:id', async (req, reply) => {
+        const { id } = req.params;
+        const { firstName, lastName, email, role, cpf, phone } = req.body;
+        try {
+            const data = await userUseCase.update({ id, firstName, lastName, email, role, cpf, phone });
+            reply.code(200).send(data);
+        } catch (error) {
+            reply.code(400).send(error);
+        }
+    });
+};
+
+function deleteUserRoute(fastify: FastifyInstance) {
+    fastify.delete<{Params: {id: string} }>('/:id', async (req, reply) => {
+        const { id } = req.params;
+        try {
+            const data = await userUseCase.delete(id);
+          return reply.send(data);
+        } catch (error) {
+            reply.code(404).send(error);
         }
     });
 };
